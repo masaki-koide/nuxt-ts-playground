95daebdc7a67c80298ec9bf8ef3484b6f1c2afb4
masaki.koide <ikasama0324@gmail.com>
2019-03-02 22:59:28 +0900

    feat: nuxt-tsのプロジェクトを作成

---
+++ components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component
+export default class HelloWorld extends Vue {
+  message: string = 'Hello world !'
+}
+</script>
